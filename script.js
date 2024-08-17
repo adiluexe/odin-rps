ac4d024b231c7e1f0f4f6dfb1b7ce6aa386baa12
adiluexe <adiluexe@gmail.com>
2024-08-17 14:19:36 +0800

    Feat: Add getComputerChoice() function
    - Randomly selects choice from choices array

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const random = Math.floor(Math.random() * choices.length);
+  return choices[random];
+}
+
+console.log(getComputerChoice());
