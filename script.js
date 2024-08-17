5473d83c1a7776efbae29dbcb03437ba4296db6f
adiluexe <adiluexe@gmail.com>
2024-08-17 14:49:48 +0800

    Feat: Add playRound() function
    - Fix prompt() running multiples times by making getHumanChoice into just a variable. I don't know why Odin instructs you to make it into a function but this is a current fix that I found works

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   const random = Math.floor(Math.random() * choices.length);
-  return choices[random];
+  return choices[random].toUpperCase();
 }
 
 console.log(getComputerChoice());
 
-function getHumanChoice(playerChoice) {
-  playerChoice = prompt("Rock, Paper, Scissors? Make your choice");
-  return playerChoice;
+let getHumanChoice = prompt("Rock, Paper, Scissors? Make your choice");
+getHumanChoice.toUpperCase();
+
+console.log(getHumanChoice);
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = getHumanChoice;
+  computerChoice = getComputerChoice();
+
+  if (
+    (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (humanChoice === "PAPER" && computerChoice === "ROCK") ||
+    (humanChoice === "SCISSORS" && computerChoice === "PAPER")
+  ) {
+    humanScore++;
+    console.log(`You win this round! ${humanChoice} beats ${computerChoice}`);
+  } else if (humanChoice == computerChoice) {
+    console.log(`It's a tie you both chose ${humanChoice}`);
+  } else {
+    computerScore++;
+    console.log(`You lose this round. ${computerChoice} beats ${humanChoice}`);
+  }
 }
 
-console.log(getHumanChoice());
+const humanSelection = getHumanChoice;
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
